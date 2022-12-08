c497a71eb754c8ff40813788df5125c38db71c64
Umuhasna <fa1033170@gmail.com>
2022-12-08 14:31:04 -0800

    My last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,9 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	write(err);
 
 	return (1);
 }
