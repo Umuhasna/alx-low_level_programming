64dde39b9d1872e490be14f9c7f9f1bffeb9a057
Umuhasna <fa1033170@gmail.com>
2022-12-08 12:05:24 -0800

    My first program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the string inside quotation
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	puts(str);
+
+	return (0);
+}
