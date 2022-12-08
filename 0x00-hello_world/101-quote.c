d0e40d0ae16c335bcbc0a6791a8c303fdbf0710b
Umuhasna <fa1033170@gmail.com>
2022-12-08 15:14:16 -0800

    Last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: Print to standard error
  *
- *   * Return: Always 1 (Success)
- *
- *    */
-
+ * Return: Always 1 (Success)
+ */
 int main(void)
-
 {
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-		write(STDOUT_FILENO, "and that piece of art is useful
-
-							\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+	return (1);
 }
