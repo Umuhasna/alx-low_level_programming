79b4f4add59e6cd94d8f0a03af64771abbb69b6b
Umuhasna <fa1033170@gmail.com>
2022-12-08 15:05:05 -0800

    Last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,25 @@
-#include <unistd.h>
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
 
 /**
- * main - Entry point
  *
- * Description: print to standard error
+ *  * main - Entry point
  *
- * Return: Always 1.
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+ *   * Return: Always 1 (Success)
+ *
+ *    */
 
-	return (1);
+int main(void)
+
+{
+
+		write(STDOUT_FILENO, "and that piece of art is useful
+
+							\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
+
 }
