869f56e11b8bf9c493244b43ca9dc80a76ecabd3
Umuhasna <fa1033170@gmail.com>
2022-12-09 14:18:36 -0800

    Don't declare variable char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print some numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar(num);
+	return (0);
+}
