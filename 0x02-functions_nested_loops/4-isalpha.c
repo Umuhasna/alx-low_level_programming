0e4d93f8c324e7e822985aff19a53ce2230ab84f
Umuhasna <fa1033170@gmail.com>
2022-12-13 15:23:10 -0800

    Checks alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * Description: Checks for alphabetic character
+ * @c: The character to be checked
+ *
+ * Return: 1 if it is a letter
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z', c >= 'A' &&  c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
