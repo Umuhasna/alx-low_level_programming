11f139ed003a61ff46a2c876003ff4e2e5090754
Umuhasna <fa1033170@gmail.com>
2022-12-13 15:42:15 -0800

    Print sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * Description: prints the sign of a number
+ * @n: the number checked its sign
+ *
+ * Return: 1 if n is greater than zero
+ * 0 if n is zero and -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	if else(n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return ("-1");
+	}
+}
