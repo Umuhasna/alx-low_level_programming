b32d589dad8306118d7db329dfb4df0fe648e638
Umuhasna <fa1033170@gmail.com>
2022-12-13 14:54:23 -0800

    Checks lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Description: Checks lowercase characters
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (_islower(c))
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
