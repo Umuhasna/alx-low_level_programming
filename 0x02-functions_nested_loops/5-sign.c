7292c3655504d827d5bd47c310134571c5b84ad3
Umuhasna <fa1033170@gmail.com>
2022-12-13 15:45:23 -0800

    Fix my code

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -18,7 +18,7 @@ int print_sign(int n)
 		_putchar(' ');
 		return (1);
 	}
-	if else(n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		_putchar(',');
@@ -30,6 +30,6 @@ int print_sign(int n)
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
-		return ("-1");
+		return (-1);
 	}
 }
