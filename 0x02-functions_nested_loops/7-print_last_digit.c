b54190e3ceb99aa8912b07eacb7274c43b926fd7
Umuhasna <fa1033170@gmail.com>
2022-12-13 16:13:56 -0800

    Write code

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Description: print the last digit of the number
+ * @i: the number
+ *
+ * Return: 0
+ */
+int print_last_digit(int i)
+{
+	int last_digit = i % 10
+		
+		return (last_digit);
+}
