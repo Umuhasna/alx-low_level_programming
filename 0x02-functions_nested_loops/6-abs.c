ed881c0a05e5df2044834361d1e194763851de64
Umuhasna <fa1033170@gmail.com>
2022-12-13 16:01:22 -0800

    create this code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * Description: computes the absolute value of an integer
+ * @int: the parameter
+ * Return: 0
+ */
+int _abs(int)
+{
+	int i;
+
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+}
