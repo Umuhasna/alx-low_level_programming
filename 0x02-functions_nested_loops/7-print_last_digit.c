8613f06d5ec3b47e726e307ec09339749683fec0
Umuhasna <fa1033170@gmail.com>
2022-12-13 16:35:28 -0800

    Change

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,13 +14,13 @@ int print_last_digit(int i)
 
 	if (i < 0)
 	{
-		last_digit = (-1 * (n % 10));
+		last_digit = (-1 * (i % 10));
 		_putchar(last_digit + '0');
 		return (last_digit);
 	}
 	else
 	{
-		last_digit = (n % 10);
+		last_digit = (i % 10);
 		_putchar (last_digit + '0');
 		return (last_digit);
 	}
