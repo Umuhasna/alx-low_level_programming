c356fe2183ba6fc8de1e881544f805a710eaa33c
Umuhasna <fa1033170@gmail.com>
2022-12-15 08:21:44 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is more about nested loop and functions
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * File - main.h
+ * @c: character to be checked for _putchar
+ *
+ * Description: header file containing the prototype of the function
+ *
+ * Return: int or void depending on the function
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
