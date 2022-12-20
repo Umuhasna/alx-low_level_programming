0782a405cf5da10b98c5f1bf5d7315586a394f09
Umuhasna <fa1033170@gmail.com>
2022-12-20 11:06:04 -0800

    create README

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * File - main.h
+ *
+ * Description: header file containing all the prototypes
+ * that are used in this directory
+ * @c: variable
+ *
+ * Return: int or void
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+
+#endif
