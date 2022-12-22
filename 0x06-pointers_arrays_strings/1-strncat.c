38bbc044e04a53015781c133976fc86cb4818e74
Umuhasna <fa1033170@gmail.com>
2022-12-22 11:02:11 -0800

    Change the while condition

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,11 +18,13 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	while (j < n)
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
