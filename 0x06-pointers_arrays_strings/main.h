efd828f63d842adb31d3a39fc782e6bf2d74409e
Umuhasna <fa1033170@gmail.com>
2022-12-22 03:00:45 -0800

    Add comment

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+/**
+ * main - contain the prototype of all
+ * the function used in this project
+ * @c: variable
+ *
+ * Return: void, int, char
+ */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 
