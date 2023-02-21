648c9f65d62c8921bb3ad0059aa6e0cf0db2c0a4
Umuhasna <fa1033170@gmail.com>
2023-02-21 14:17:02 -0800

    Remove the trailing whitespace that was on line number 7

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha -checks for alphabetic character
  * @c: the character to check
  *
- * Return: 1 if c is a letter, lowercase or uppercase, 
+ * Return: 1 if c is a letter, lowercase or uppercase,
  * 0 otherwise
  */
 int _isalpha(int c)
