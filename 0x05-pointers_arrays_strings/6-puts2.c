6e0af946e5f8f59ee979c8bdeee2cdeb4a862f6a
Umuhasna <fa1033170@gmail.com>
2023-03-01 08:59:40 -0800

    Fix the code so that it is not going to print what is after the '0'

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,19 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i, j;
+	
+	i = 0;
+	while (str[i] != '\0')
 	{
-		_putchar(*str);
-		str+=2;
+		i++;
+	}
+	
+	j = 0;
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j+=2;
 	}
 	_putchar('\n');
 }
