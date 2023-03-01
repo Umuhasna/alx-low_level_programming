e15aa9ae7b88c3b7eada6ebc7d905e6db2321da7
Umuhasna <fa1033170@gmail.com>
2023-03-01 09:06:49 -0800

    Remove the trailing spaces on line 13 & 19 and add spaces around +=

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,18 +10,18 @@
 void puts2(char *str)
 {
 	int i, j;
-	
+
 	i = 0;
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	
+
 	j = 0;
 	while (j < i)
 	{
 		_putchar(str[j]);
-		j+=2;
+		j += 2;
 	}
 	_putchar('\n');
 }
