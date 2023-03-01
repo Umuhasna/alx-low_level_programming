6e93df73f24cf5059cc35bd979742e0c37117a4c
Umuhasna <fa1033170@gmail.com>
2023-03-01 15:56:37 -0800

    Fix compilation error by removing the line 16 which was containing statement with no effect

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,9 +11,9 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; )
 	{
-		dest[i];
+		i++;
 	}
 	for (j = 0; dest[j] != '\0'; j++)
 	{
