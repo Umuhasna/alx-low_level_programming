57662e775ca04bd3014f733b871288f37c4eb0c0
Umuhasna <fa1033170@gmail.com>
2023-03-08 10:20:26 -0800

    Fix - Add a condition to check if the first character is a letter or not

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -26,7 +26,10 @@ char *cap_string(char *str)
 		}
 		if (i == 0)
 		{
-			str[i] = str[i] - 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+			}
 		}
 	}
 	return (str);
