12c727fbd5bb7d19cca9d44662c010a60396b6f0
Umuhasna <fa1033170@gmail.com>
2023-03-16 10:59:34 -0700

    Fix correct the indentation, add else statement for the malloc and open the curly bracket for the for loops

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,31 +21,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 
 	for (len1 = 0; s1[len1]; len1++)
+	{
+	}
 	for (len2 = 0; s2[len2]; len2++)
+	{
+	}
 
 	if (n < len2)
 		constr = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		constr = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	constr = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (constr == NULL)
 		return (NULL);
 
-	for (index12 = 0; index12 < len1; index12++)
+	for (index12 = 0; index12 < len1; index12++, index++)
 	{
 		constr[index] = s1[index12];
-		index++;
 	}
 	if (n < len2)
-		for (index12 = 0; index12 < n; index12++)
+		for (index12 = 0; index12 < n; index12++, index++)
 		{
 			constr[index] = s2[index12];
-			index++;
 		}
 	else
-		for (index12 = 0; index12 < len2; index12++)
+		for (index12 = 0; index12 < len2; index12++, index++)
 		{
 			constr[index] = s2[index12];
-			index++;
 		}
 	constr[index] = '\0';
 	return (constr);
