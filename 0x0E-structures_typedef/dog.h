543ec471bbb5274512ffe9b11c430c47026b331c
Umuhasna <fa1033170@gmail.com>
2023-03-20 06:22:31 -0700

    Add description about the struct dog following betty Data structure Documentation

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H_
 #define DOG_H_
 
-struct dog {
+/**
+ * struct dog - record info about a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ *
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
