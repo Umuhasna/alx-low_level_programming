34b4ecaaf6c2656e8ab0a25964cef3a7c492b6a4
Umuhasna <fa1033170@gmail.com>
2023-03-21 16:31:05 -0700

    Fix Store the copy of the string name and owner in the allocated structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,26 @@
 #include "dog.h"
 
 /**
+ * _strcpy - copies the string pointed to by src, including
+ * the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: the buffer to copy to
+ * @src: the string to copy
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: pointer to name
  * @age: age
@@ -14,11 +34,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	ptr = malloc(sizeof(dog_t));
 	if (ptr == NULL)
+	{
+		free(ptr);
 		return (NULL);
+	}
 
-	ptr->name = name;
+	ptr->name = malloc(sizeof(ptr->name));
+	if (ptr->name == NULL)
+		free(ptr->name);
+
+	ptr->owner = malloc(sizeof(ptr->owner));
+	if (ptr->owner == NULL)
+		free(ptr->owner);
+
+	ptr->name = _strcpy(ptr->name, name);
 	ptr->age = age;
-	ptr->owner = owner;
+	ptr->owner = _strcpy(ptr->owner, owner);
 
 	return (ptr);
 }
