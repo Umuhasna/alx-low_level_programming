d1d21a176a83cd172369f3055b307d8641bdc871
Umuhasna <fa1033170@gmail.com>
2023-04-04 17:17:23 -0700

    Fix update new_node at the begininning

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
+	new_node->next = NULL;
+
 	current = *head;
 	if (idx == 0)
 	{
@@ -32,7 +35,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (count == idx - 1)
 		{
-			new_node->n = n;
 			new_node->next = current->next;
 			current->next = new_node;
 			return (new_node);
