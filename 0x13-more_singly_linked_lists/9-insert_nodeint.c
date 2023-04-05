cf9c2e3a76d924f8e13ad3090961986a425001fa
Umuhasna <fa1033170@gmail.com>
2023-04-04 17:14:17 -0700

    Fix change much of the code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,17 +28,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 		return (new_node);
 	}
-	while (count < idx - 1)
+	while (current != NULL && count < idx)
 	{
-		if (current == NULL || current->next == NULL)
-			return (NULL);
-
-		current = current->next;
+		if (count == idx - 1)
+		{
+			new_node->n = n;
+			new_node->next = current->next;
+			current->next = new_node;
+			return (new_node);
+		}
+		else
+			current = current->next;
 		count++;
 	}
-	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
 
-	return (new_node);
+	return (NULL);
 }
