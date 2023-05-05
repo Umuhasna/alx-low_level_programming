47a127219084aff2594c50007a562d92768268dc
Umuhasna <fa1033170@gmail.com>
2023-05-05 03:16:14 +0300

    Write a function that sets the value of a bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: pointer to unsigned long integer
+ * @index: index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int mask, size;
+
+	size = sizeof(unsigned long int);
+	if (index >= size * 8)
+		return (-1);
+
+	mask = ~(1ul << index);
+	*n = *n & mask;
+
+	return (1);
+}
