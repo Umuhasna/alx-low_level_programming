9c19e3b3c0093cb1a2fd028c0c170b303af1891b
Umuhasna <umuhasna123@gmail.com>
2023-06-20 12:05:09 +0300

    Write a function that prints all the elements of a dlistint_t list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num_of_nodes = 0;
-	const dlistint_t *node;
 
-	for (node = h; node; node = node->next)
+	while (h)
 	{
-		printf("%d\n", node->n);
+		printf("%d\n", h->n);
+
+		h = h->next;
 		num_of_nodes++;
 	}
 
