7a0539740327699f891e07a2d0cc3a99f62fdad6
Umuhasna <umuhasna123@gmail.com>
2023-06-20 12:11:03 +0300

    Fix betty documentation style

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - returns the number of elements in a linked dlistint_t list
  * @h: pointer to the first node
  *
- * return: the number of elements in a linked dlistint_t list
+ * Return: the number of elements in a linked dlistint_t list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
